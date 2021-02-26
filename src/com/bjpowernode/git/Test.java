1e251abbe2294745c1e8b71b4f882e9e5c9bbd2a
guan <guan@qq.com>
2021-02-26 12:26:26 +0800

    2.新创建的分支

--- src/com/bjpowernode/git/Test.java
+++ src/com/bjpowernode/git/Test.java
@@ -3,5 +3,6 @@ package com.bjpowernode.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("新分支写的代码！");
     }
 }
